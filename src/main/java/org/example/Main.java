0642a861b94959ad34b0fac049bf4881f67f53d0
Sakaev Ruslan <hardist@mail.ru>
2023-07-25 22:35:14 +0300

    Создал массив сотрудников

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee("Иванов Сергей Петрович", "Manager", "+7 123-456-78-90", 5000.0, 35);
-        System.out.println(employee.toString());
+        // Создаем массив для хранения 5 сотрудников
+        Employee[] employees = new Employee[6];
+
+        // Создаем объекты сотрудников и помещаем их в массив
+        employees[0] = new Employee("Иванов Сергей Иванович", "Developer", "+7-345-67-89", 50000, 30);
+        employees[1] = new Employee("Петров Николай Васильевич", "Designer", "+7-987-654-43-21", 45000, 35);
+        employees[2] = new Employee("Тёркин Дмитрий Петрович", "Manager", "+7-555-123-45-56", 60000, 50);
+        employees[3] = new Employee("Лаврова Анна Сергеевна", "QA Engineer", "+7-111-222-33-33", 48000, 42);
+        employees[4] = new Employee("Маркин Лев Дмитриевич", "Developer", "455-56-66", 52000, 28);
+
+        // Выводим информацию о каждом сотруднике
+        for (Employee employee : employees) {
+            System.out.println(employee.toString());
+            System.out.println("--------------------");
+        }
     }
 }
