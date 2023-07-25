bce829cea272ccf4ee784832a236d313a3f03d09
Sakaev Ruslan <hardist@mail.ru>
2023-07-25 22:29:41 +0300

    Добавил метод вывода с форматированием строк

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee("John Doe", "Manager", "+1 123-456-7890", 5000.0, 35);
+        Employee employee = new Employee("Иванов Сергей Петрович", "Manager", "+7 123-456-78-90", 5000.0, 35);
         System.out.println(employee.toString());
     }
 }
